71c56f8087ca4da1a1b923e5ce6dff0ddd794c7f
gla2s <95167241+gla2s@users.noreply.github.com>
2022-05-08 09:48:27 +0300

    Add files via upload

---
+++ alanlar.py
@@ -0,0 +1,26 @@
+
+print("      Dairenin Alanı") 
+π= float (input ("π'yi giriniz: "))
+r= float  (input ("r'yi giriniz: "))
+S=π*r**2
+print("Dairenin Alani:") ; print(S)
+  
+  
+  
+  
+print("      Üçgenin Alanı")  
+a= float (input ("Taban Uzunluğunu giriniz: "))
+h= float (input ("Yüksekliği giriniz: "))
+A=a*h/2
+print ("Üçgenin Alanı: ") ; print (A)
+
+
+
+print("      Yamuğun Alanı")
+a= float (input ("Üst Tabanı giriniz: "))
+b= float (input ("Alt Tabanı giriniz: "))
+h= float (input ("Yüksekliği giriniz: "))
+A= (a+b)*h/2
+print ("Yamuğun Alanı:") ; print (A)
+
+print("                Powered by Ali Yusuf Karış") 
